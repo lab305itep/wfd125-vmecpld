4d5756ef86f510ab29467d8591c11effe1ae7ef3
Dmitry Svirida <dmitry.svirida@itep.ru>
2014-09-04 18:10:37 +0300

    flashio module sketched

--- flash_io.v
+++ flash_io.v
@@ -1,40 +1,82 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: ITEP
+// Engineer: SvirLex
 // 
-// Create Date:    14:08:52 09/04/2014 
-// Design Name: 
+// Create Date:    19:11:52 09/01/2014 
+// Design Name:    vmecpld
 // Module Name:    flash_io 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
+// Project Name:   wfd125
+// Target Devices: xc2c128-7-VQ100
 //
-// Dependencies: 
-//
-// Revision: 
 // Revision 0.01 - File Created
 // Additional Comments: 
+//		On write to data register, generates 8 FCLK pulses, shifts output data
+// 	on negative edges, latches input data on positive edges
 //
 //////////////////////////////////////////////////////////////////////////////////
 module flash_io(
+		// sustem clock 125 MHz
     input CLK,
+		// 1 if CPLD is flash master
     input ENABLE,
+		// data write strobe
     input WS,
+		// data read strobe
     input RS,
+		// data lines to VME
     inout [7:0] DATA,
+		// serial input from FLASH
 	 input SI,
+		// serial output to FLASH
 	 output SO,
+		// serial clock to FLASH
 	 output FCK
     );
 
-	reg [7:0] OSREG;
+	// Output shift register
+	reg [7:0] OSREG = 0;
+	// input shift register
+	reg [7:0] ISREG = 0;
+	// serial clock, inactive high, CLK/2 = 62.5 MHz
+	reg FCLK = 1;
+	// state machine variable
+	integer i = 0;
 
-	assign DATA = (RS) ? OSREG : 8'hzz;
+	// read serial input register
+	assign DATA = (RS) ? ISREG : 8'hzz;
+	// serial data MSB first, when enabled
+	assign SO = (ENABLE) ? OSREG : 1'bz;
+	// serial clock, when enabled
+	assign FCK = (ENABLE) ? FCLK : 1'bz;
 	
 	always @(posedge CLK) begin
-		if (WS) OSREG <= DATA;
+		// start on write to data register
+		if (i == 0) begin
+			if (WS) begin 
+				// set data, first FCLK 1->0 transition
+				OSREG <= DATA;
+				FCLK <= 0;
+				i <= 1;
+			end
+		end
+		// odd transitions 0->1, latch input MSB first
+		// 1 3 5 7 9 11 13 15
+		else if (i & 1) begin
+			ISREG <= {ISREG[7:1], SI}; 
+			FCLK <= 1;
+			if (i == 15) i <= 0;
+			else i <= i+1;
+		end
+		// even transitions 1->0, change output MSB first
+		// 2 4 6 8 10 12 14
+		else begin
+			OSREG <= {OSREG[6:0], 0};
+			FCLK <= 0;
+			i <= i+1;
+		end
+		
+
 	end
 
 endmodule
--- vmecpld.v
+++ vmecpld.v
@@ -4,20 +4,18 @@
 // Engineer: SvirLex
 // 
 // Create Date:    19:11:52 09/01/2014 
-// Design Name: 
+// Design Name:    vmecpld
 // Module Name:    vmecpld 
 // Project Name:   wfd125
 // Target Devices: xc2c128-7-VQ100
-// Tool versions: 
-// Description: 
 //
-// Dependencies: 
-//
-// Revision: 
 // Revision 0.01 - File Created
 // Additional Comments: 
 //
-// Registers:
+// Registers: to be read/written as lower (odd) bytes with A16/D16 transfers
+//
+//		BASE = A000 + (SERIAL << 4)
+//
 //		BASE+0: CSR[7:0] (RW) Control & Status register
 //					CSR0 (RW) FLASH_CS, 1 - CS asserted if FLASH enabled, default 0
 //					CSR1 (RW) FLASH_ENB, 1 - FLASH access enabled from CPLD side, default 0
@@ -40,44 +38,74 @@
 
 
 module vmecpld(
+		// VME DATA (lower 8 bits)
     inout [7:0] XD,
+		// VME ADDR (lower 16 bits)
     input [15:0] XA,
+		// VME address modifier
     input [5:0] XAM,
+		// VME geographical address (useless so far)
     input [5:0] XGA,
+		// VME address strobe
     input XAS,
+		// VME data strobes
     input [1:0] XDS,
+		// VME write (low)
     input XWRITE,
+		// VME reset (low)
     input XRESET,
+		// interrupt service (inactive so far)
     input IACKPASS,
     input XIACK,
     input XIACKIN,
     output XIACKOUT,
+		// VME data acknowledge (low)
     output XDTACK,
+		// enable DTACK from tristate (low)
     output XDTACKOE,
+		// buffer data direction (output to VME -- low)
     output DDIR,
+		// 125 MHz clock
 	 input CPLDCLK,
+		// power-on reset
     input CRST,
+		// test points
     output [5:1] TP,
+		// FLASH clock
     output FLASHCLK,
-    input FLASHCS,
+		// FLASH select (low)
+    output FLASHCS,
+		// FLASH data
     inout [3:0] FLASHD,
+		// connection to main FPGA
     input [7:0] C2X,
+		// configuration mode to main FPGA
     output [1:0] M,
+		// DONE from FPGA chain
     input DONE,
+		// PROG to FPGA chain
     output PROG,
+		// INIT from FPGA chain
     input INIT
     );
 
+// serial and butch numbers (externally generated)
 `include "serial.vh"
+
+// Current number of active registers (<= 8)
 localparam NREGS = 5;
 
 	wire CLK;
-	reg [7:0] CSR = 8'h00;
+	// module addressed bit
 	reg ADS = 0;
+	// clocked data strobe and delayed
 	reg DDS = 0;
 	reg DDST = 0;
+	// Write and read strobes
 	wire [NREGS-1:0] WS;
 	wire [NREGS-1:0] RS;
+	// CSR
+	reg [7:0] CSR = 8'h00;
 	
 	assign CLK = CPLDCLK;
 	assign TP[1] = ADS;
@@ -88,43 +116,65 @@ localparam NREGS = 5;
 
 	assign M = 2'b11;
 	assign PROG = 1'b1;
-	assign FLASHCLK = 1'bz;
 	assign XIACKOUT = XIACKIN;
-	assign FLASHD = 4'hz;
+
+	// Flash CS is driven by CSR0 when enabled
+	assign FLASHCS = (CSR[1]) ? !CSR[0] : 1'bz;
+	// keep WP and HOLD HIGH
+	assign FLASHD[3:2] = 2'b11;
+	
+	// reply with DTACK=0 to DS0, leave driven as 1 one CLK after deasserting
 	assign XDTACK = (DDS) ? 0 : ( (DDST) ? 1 : 1'bz);
 	assign XDTACKOE = (DDS || DDST) ? 0 : 1'bz;
+	// buffer direction OUT on read operations
 	assign DDIR = (DDS && XWRITE) ? 1 : 1'bz;
 
+	// generate rad and write strobes
 	genvar i;
    generate
       for (i=0; i < NREGS; i=i+1) 
       begin: GWS
+			// write strobe is 1 CLK on leading edge of DS0
          assign WS[i] = !XWRITE && !DDST && DDS && (XA[3:1] == i);
-			assign RS[i] = DDS && XWRITE && (XA[3:1] == i);
+			// read strobe is for the whole duration of DS0
+			assign RS[i] = XWRITE && DDS && (XA[3:1] == i);
       end
    endgenerate
 
+// Read registers in this top module
+	// CSR
 	assign XD = (RS[0]) ? {CSR} : 8'hzz;
+	// Serial number
 	assign XD = (RS[3]) ? {SERIAL} : 8'hzz;
+	// Batch number
 	assign XD = (RS[4]) ? {BATCH} : 8'hzz;
 	
 
 	always @(posedge CLK) begin
+		
+		// if regular A16 address matches
 		if (!XAS && (XAM == 6'h2D || XAM == 6'h29) && XIACK && XA[0] && XA[15:4] == (12'hA00 + SERIAL) ) ADS <= 1;
+		// if DS0 asserted (ignore DS1)
 		if (ADS && !XDS[0]) begin
 			DDS <= 1;
 		end else begin
 			DDS <= 0;
 		end
+		// delayed DDS
 		DDST <= DDS;
+		// finish cycle at trailing edge of DS0
 		if (DDST && !DDS) begin
 			ADS <= 0;
 		end
+		
+		// write CSR
 		if (WS[0]) begin
 			CSR <= XD;
 		end
+		
 	end
 	
+	// Flash connection module
 	flash_io FLASHIO (
 		.CLK(CLK),
 		.ENABLE(CSR[1]),
